07b96e8c601a1f770e5bdab936eccc1f65dbb0bb
DapperReaper <mtfa2@yahoo.com>
2018-06-19 14:26:05 -0700

    added login screen, added test button to test score display(need to write code for test button still)

---
+++ Assets/LoginBtn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+
+public class LoginBtn : MonoBehaviour {
+
+    public Button logbtn;
+    public InputField User;
+    private string temp;
+    
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = logbtn.GetComponent<Button>();
+        btn.onClick.AddListener(Clicking);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Clicking()
+    {
+        //add code to grab username here
+        StreamWriter injection = new StreamWriter("Username.txt", true);
+        temp = User.text;
+        injection.Write(temp);
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/TestBtn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TestBtn : MonoBehaviour {
+
+    public Button backbtn;
+    public VerticalLayoutGroup vert;
+    public GUIContent con;
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = backbtn.GetComponent<Button>();
+        
+        btn.onClick.AddListener(Clicking);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Clicking()
+    {
+        VerticalLayoutGroup vrt = vert.GetComponent<VerticalLayoutGroup>();
+       
+        for (int x=0; x<100; x++)
+        {
+            //add items to layout here
+
+        }
+    }
+}
