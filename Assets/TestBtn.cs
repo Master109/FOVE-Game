43d58efd86aa5e83da1d9e410f3479c27f30867d
DapperReaper <mtfa2@yahoo.com>
2018-06-20 10:22:30 -0700

    removed test button

--- Assets/TestBtn.cs
+++ Assets/TestBtn.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 
 public class TestBtn : MonoBehaviour {
 
-    public Button backbtn;
+    public Button testbtn;
     public VerticalLayoutGroup vert;
     public GUIContent con;
+    public Text TestText;
+    private Vector3 autoLocalScale;
     // Use this for initialization
     void Start()
     {
-        Button btn = backbtn.GetComponent<Button>();
+        Button btn = testbtn.GetComponent<Button>();
         
         btn.onClick.AddListener(Clicking);
+        autoLocalScale = new Vector3(1.0f, 1.0f, 1.0f);
     }
 
     // Update is called once per frame
@@ -25,11 +28,25 @@ public class TestBtn : MonoBehaviour {
     void Clicking()
     {
         VerticalLayoutGroup vrt = vert.GetComponent<VerticalLayoutGroup>();
-       
-        for (int x=0; x<100; x++)
+        Text textbox = TestText.GetComponent<Text>();
+        string test = "";
+        for(int x=0; x<100; x++)
         {
             //add items to layout here
+            test = "Flack" + "\t" + x + "\t" + "test text" + "\n ";
+            textbox.text = test;
+            AddItem(textbox);
+            //textbox.transform.parent = vrt.transform;
+            //vrt.GetComponentsInChildren<Text>();
 
-        }
+        } 
+        
+    }
+    public void AddItem(Text item)
+    {
+        
+        item.transform.SetParent(vert.transform, false);
+        item.transform.localScale = autoLocalScale;
+        item.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
     }
 }
