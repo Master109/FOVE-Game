ce38c5abc86540e1911db72a7789e6465e2ed889
DapperReaper <mtfa2@yahoo.com>
2018-06-21 11:16:54 -0700

    Added text files for storing score and the data we collect added a script to pause the game(dunno if gilly needs this or not)

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PauseGame : MonoBehaviour {
+
+    bool IsPause = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsPause = !IsPause;
+            if (IsPause)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+        }
+	}
+    private void OnGUI()
+    {
+        if(IsPause)
+        {
+            SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+        }
+    }
+}
