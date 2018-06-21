501eb701c17e889da22e17f32722fca10d57f157
DapperReaper <mtfa2@yahoo.com>
2018-06-21 12:56:24 -0700

    added scripts to handle some of the back end data

---
+++ Assets/Scripts/ReadScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class ReadScore : MonoBehaviour {
+    public StreamReader Probe = new StreamReader("Score.txt");
+    public string score;
+    public Text ScoreBoard;
+	// Use this for initialization
+	void Start ()
+    {
+        Text txt = ScoreBoard.GetComponent<Text>();
+        score = Probe.ReadLine();
+        txt.text = score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WriteData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class WriteData : MonoBehaviour {
+
+    public StreamWriter Injector;
+    public StreamReader ProbeName;
+    public StreamReader ProbeScore;
+    private string DataEntry;
+
+    // Use this for initialization
+    void Start()
+    {
+        Writing();
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Writing()
+    {
+        Injector = new StreamWriter("Data.txt", true);
+        ProbeName = new StreamReader("Username.txt");
+        ProbeScore = new StreamReader("Score.txt");
+        //Injector.AutoFlush = true;
+        DataEntry = ProbeName.ReadLine() + "\t" + ProbeScore.ReadLine() + "\n"; //add other data here before end line character
+        Injector.Write(DataEntry);
+        Injector.Flush();
+        Injector.Close();
+        ProbeName.Close();
+        ProbeScore.Close();
+    }
+}
