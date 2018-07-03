33b9e0e2c9986d6d68319b541a4c1f81d0cfc64d
DapperReaper <mtfa2@yahoo.com>
2018-07-03 13:00:16 -0700

    implimented example code to add clicking sound using temp sound effect, can change later

--- Assets/Scripts/Buttons/ClickSoundBtn.cs
+++ Assets/Scripts/Buttons/ClickSoundBtn.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using ClassExtensions;
 
-public class ClickSoundBtn :Button {
+
+
+public class ClickSoundBtn : MonoBehaviour
+{
 
     public AudioSource Doink;
+    public Button Bonk;
 
+    private void Start()
+    {
+        Button btn = Bonk.GetComponent<Button>();
+        btn.onClick.AddListener(Clicking);
+    }
+
+
+    /*
     public override void OnPointerClick(PointerEventData eventData)
     {
         base.OnPointerClick(eventData);
@@ -15,4 +28,10 @@ public class ClickSoundBtn :Button {
         Doink.Play();
     }
     
+    */
+    void Clicking()
+    {
+        Doink = GetComponent<AudioSource>();
+        Doink.Play();
+    }
 }
