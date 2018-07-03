2aac29d6805e5afd1f979b634644eaa417768cf5
DapperReaper <mtfa2@yahoo.com>
2018-07-03 12:39:07 -0700

    started writing script for button click sound

---
+++ Assets/Scripts/Buttons/ClickSoundBtn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ClickSoundBtn :Button {
+
+    public AudioSource Doink;
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        Doink = GetComponent<AudioSource>();
+        Doink.Play();
+    }
+    
+}
