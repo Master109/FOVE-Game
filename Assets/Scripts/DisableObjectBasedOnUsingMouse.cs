e5ae8b831b6e46ab2c90adfd35b4b1e1c2c90d48
Gilead Cosman <gcosman@rhyme.UCSD.edu>
2018-07-18 17:17:35 -0800

    Moved pause and menu buttons for mouse version

---
+++ Assets/Scripts/DisableObjectBasedOnUsingMouse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableObjectBasedOnUsingMouse : MonoBehaviour
+{
+	public bool activeIfUsingMouse;
+	
+	void Start ()
+	{
+		gameObject.SetActive(activeIfUsingMouse == ApplicationUser.GetInstance().useMouse);
+	}
+}
